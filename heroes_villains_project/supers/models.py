b8c8f43728093503c17b1eca8033e589189658d2
chrisbelles <chris.belles1@gmail.com>
2022-11-24 10:50:32 -0600

    created the supers model

--- heroes_villains_project/supers/models.py
+++ heroes_villains_project/supers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class SuperType(models.Model):
+    name = models.CharField(max_length=255)
+    alter_ego = models.CharField(max_length=255)
+    primary_ability = models.CharField(max_length=255)
+    secondary_ability = models.CharField(max_length=255)
+    catch_phrase =  models.CharField(max_length=255)
+    super_type = models.ForeignKey(SuperType, on_delete=models.CASCADE)
